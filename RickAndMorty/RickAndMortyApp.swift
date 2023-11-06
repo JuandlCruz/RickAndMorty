8ea623397a1cb15a3b3cb247d8f5f93432097b82
JuandlCruz <juandelacrouz@gmail.com>
2023-11-06 22:46:13 +0100

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Juan on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
